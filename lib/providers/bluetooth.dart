d42d10fc51323835d150a8d3b29d285b7e196441
GioPan04 <gioele@pannetto.com>
2023-07-24 15:08:13 +0200

    Using custom logger

--- lib/providers/bluetooth.dart
+++ lib/providers/bluetooth.dart
@@ -4,24 +4,31 @@ import 'package:bluez/bluez.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:ui/dialogs/confirm_device.dart';
+import 'package:ui/providers/logger.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 final bluetoothProvider = FutureProvider<BlueZClient>((ref) async {
+  final logger = ref.read(loggerProvider);
+
   final client = BlueZClient();
   await client.connect();
-  final agent = OCSAgent();
+  final agent = OCSAgent(logger: logger);
+
   await client.registerAgent(agent);
   await client.requestDefaultAgent();
+  logger.i("[Bluetooth]: Registered custom agent");
 
   return client;
 });
 
 class OCSAgent extends BlueZAgent {
   final GlobalKey<NavigatorState> _navigatorKey;
+  final Logger logger;
 
-  OCSAgent() : _navigatorKey = navigatorKey;
+  OCSAgent({required this.logger}) : _navigatorKey = navigatorKey;
 
   int _generatePin() {
     final random = Random();
@@ -31,29 +38,38 @@ class OCSAgent extends BlueZAgent {
   @override
   Future<BlueZAgentPinCodeResponse> requestPinCode(BlueZDevice device) async {
     final pin = _generatePin();
-    print("Use pin $pin");
+    logger
+        .i("[OCSAgent]: ${device.address} requested pin for connection. $pin");
     return BlueZAgentPinCodeResponse.success('$pin');
   }
 
   @override
   Future<BlueZAgentResponse> displayPinCode(
-      BlueZDevice device, String pinCode) async {
-    print('DisplayPinCode: PinCode $pinCode');
+    BlueZDevice device,
+    String pinCode,
+  ) async {
+    logger.i("[OCSAgent]: ${device.address} sent pin for connection. $pinCode");
     return BlueZAgentResponse.success();
   }
 
   @override
   Future<BlueZAgentPasskeyResponse> requestPasskey(BlueZDevice device) async {
-    print("RequestPassKey");
     final pin = _generatePin();
-    print("Use pin $pin");
+    logger.i(
+      "[OCSAgent]: ${device.address} requested pass key for connection. $pin",
+    );
     return BlueZAgentPasskeyResponse.success(pin);
   }
 
   @override
   Future<BlueZAgentResponse> displayPasskey(
-      BlueZDevice device, int passkey, int entered) async {
-    print('DisplayPasskey: $passkey');
+    BlueZDevice device,
+    int passkey,
+    int entered,
+  ) async {
+    logger.i(
+      "[OCSAgent]: ${device.address} sent pass key for connection: $passkey. Device sent: $entered",
+    );
     return BlueZAgentResponse.success();
   }
 
@@ -62,7 +78,10 @@ class OCSAgent extends BlueZAgent {
     BlueZDevice device,
     int passkey,
   ) async {
-    print('Request connection with passkey $passkey');
+    logger.i(
+      '[OCSAgent]: ${device.address} requested connection with passkey $passkey',
+    );
+
     bool res = await showDialog(
       context: _navigatorKey.currentContext!,
       builder: (context) => ConfirmDeviceDialog(
@@ -70,12 +89,16 @@ class OCSAgent extends BlueZAgent {
         pin: "$passkey",
       ),
     );
+
+    logger.i(
+      '[OCSAgent]: Connection with ${device.address} ${res ? 'accepted' : 'rejected'}',
+    );
     return res ? BlueZAgentResponse.success() : BlueZAgentResponse.rejected();
   }
 
   @override
   Future<BlueZAgentResponse> requestAuthorization(BlueZDevice device) async {
-    print("requestAuthorization");
+    logger.i('[OCSAgent]: ${device.address} request authorization. Accepted');
     return BlueZAgentResponse.success();
   }
 
@@ -84,7 +107,7 @@ class OCSAgent extends BlueZAgent {
     BlueZDevice device,
     BlueZUUID uuid,
   ) async {
-    print("authorizeService");
+    logger.i('[OCSAgent]: ${device.address} requested service $uuid. Accepted');
     return BlueZAgentResponse.success();
   }
 }
---
+++ lib/providers/logger.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+final loggerProvider = Provider<Logger>(
+  (ref) => Logger(
+    printer: SimplePrinter(),
+  ),
+);
