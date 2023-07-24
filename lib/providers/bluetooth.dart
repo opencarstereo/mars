9766433283f2ceaf2a8bfd4760fc7b914a217167
GioPan04 <gioele@pannetto.com>
2023-07-24 15:09:27 +0200

    Removed unused imports

--- lib/providers/bluetooth.dart
+++ lib/providers/bluetooth.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
+
 import 'package:bluez/bluez.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:ui/dialogs/confirm_device.dart';
