2df0f4156a34254145e873369a9e9b8370f19fe1
GioPan04 <gioele@pannetto.com>
2023-07-24 15:36:23 +0200

    Player in home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ui/providers/bluetooth.dart';
+import 'package:ui/widgets/player.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -13,17 +14,21 @@ class HomePage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Open Car Stereo'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/player'),
-            child: const Text('Player'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/settings'),
-            child: const Text('Settings'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const PlayerWidget(),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/player'),
+              child: const Text('Player'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/settings'),
+              child: const Text('Settings'),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/player.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ui/models/player.dart';
+import 'package:ui/providers/player.dart';
+
+class PlayerWidget extends ConsumerWidget {
+  const PlayerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    final player = ref.watch(playerProvider).value;
+
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(
+          color: Colors.white24,
+          width: 2.0,
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            player?.title ?? '--',
+            style: theme.textTheme.headlineLarge,
+          ),
+          Text(
+            player?.artist ?? '--',
+            style: theme.textTheme.titleLarge,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () => ref.read(playerProvider.notifier).previous(),
+                icon: const Icon(
+                  Icons.skip_previous,
+                ),
+              ),
+              const SizedBox(
+                width: 32,
+              ),
+              IconButton(
+                iconSize: 48,
+                onPressed: () {
+                  if (player?.status == PlayerStatus.playing) {
+                    ref.read(playerProvider.notifier).pause();
+                  } else if (player?.status == PlayerStatus.paused ||
+                      player?.status == PlayerStatus.stopped) {
+                    ref.read(playerProvider.notifier).play();
+                  }
+                },
+                icon: Icon(
+                  player?.status == PlayerStatus.playing
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                ),
+              ),
+              const SizedBox(
+                width: 32,
+              ),
+              IconButton(
+                onPressed: () => ref.read(playerProvider.notifier).next(),
+                icon: const Icon(
+                  Icons.skip_next,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
